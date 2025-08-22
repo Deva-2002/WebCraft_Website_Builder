6478fe658f382da555fcf5bf18e0b170f89f2e93
Deva-2002 <mikkuvakkom391@gmail.com>
2025-08-22 14:04:57 +0530

    added the claude_api

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import dotenv from "dotenv";
+dotenv.config();
+const CLAUDE_API_KEY = process.env.CLAUDE_API_KEY;
+import Anthropic from "@anthropic-ai/sdk";
+const client = new Anthropic({
+    // defaults to process.env["ANTHROPIC_API_KEY"]
+    apiKey: CLAUDE_API_KEY,
+});
+//till part 3 done
+async function main() {
+    await client.messages.stream({
+        messages: [{ role: 'user', content: "Hello" }],
+        model: 'claude-opus-4-1-20250805',
+        max_tokens: 1024,
+    }).on('text', (text) => {
+        console.log(text);
+    });
+}
+main();
 //# sourceMappingURL=index.js.map
